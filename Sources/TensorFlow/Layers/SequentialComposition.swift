4efb66610849482f3b3aee3d04691df632a27add
Dan Zheng <danielzheng@google.com>
2020-05-13 12:23:37 -0700

    [WIP]

--- Sources/TensorFlow/Layers/SequentialComposition.swift
+++ Sources/TensorFlow/Layers/SequentialComposition.swift
@@ -12,33 +12,133 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/*
+extension Collection where Self: Differentiable, Element: Differentiable {
+  @differentiable(wrt: (self, initialResult))
+  public func differentiableReduce<Result: Differentiable>(
+    _ initialResult: Result,
+    _ nextPartialResult: @differentiable (Result, Element) -> Result
+  ) -> Result {
+    reduce(initialResult, nextPartialResult)
+  }
+
+  @usableFromInline
+  @derivative(of: differentiableReduce)
+  internal func _vjpDifferentiableReduce<Result: Differentiable>(
+    _ initialResult: Result,
+    _ nextPartialResult: @differentiable (Result, Element) -> Result
+  ) -> (
+    value: Result,
+    pullback: (Result.TangentVector)
+      -> (TangentVector, Result.TangentVector)
+  ) {
+    var pullbacks:
+      [(Result.TangentVector) -> (Result.TangentVector, Element.TangentVector)] =
+        []
+    let count = self.count
+    pullbacks.reserveCapacity(count)
+    var result = initialResult
+    for element in self {
+      let (y, pb) =
+        valueWithPullback(at: result, element, in: nextPartialResult)
+      result = y
+      pullbacks.append(pb)
+    }
+    return (
+      value: result,
+      pullback: { tangent in
+        var resultTangent = tangent
+        var elementTangents = TangentVector()
+        elementTangents.base.reserveCapacity(count)
+        for pullback in pullbacks.reversed() {
+          let (newResultTangent, elementTangent) = pullback(resultTangent)
+          resultTangent = newResultTangent
+          elementTangents.base.insert(elementTangent, at: 0)
+        }
+        return (elementTangents, resultTangent)
+      }
+    )
+  }
+}
+*/
+
+
 /// The sequential composition of the elements of some base collection of
 /// layers.
 ///
 /// The first element of the base collection is applied first by the composite,
 /// so it is the inner call of the composition or the last in the sequence of
 /// composed layers in “f ∘ g” notation.
-public struct SequentialComposition<Base: Collection>
-  where Base.Element : Layer, Base.Element.Input == Base.Element.Output
+public struct SequentialComposition<Base: Collection>: Differentiable
+where Base: Differentiable, Base.Element: Layer, Base.Element.Input == Base.Element.Output
 {
+  public typealias TangentVector = [Base.Element.TangentVector]
+
   /// The layers to be composed.
-  private let base: Base
+  public var base: Base
 
   /// Creates an instance that composes the elenents of `base` in order, such
   /// that the first element of `base` is applied first by the composite.
   public init(_ base: Base) {
     self.base = base
   }
-  
+
   /// Performs the composed layer application.
-  @differentiable
+  @differentiable(wrt: (self, input))
   public func callAsFunction(
     _ input: Base.Element.Input
   ) -> Base.Element.Output {
     var r = input
     for l in base { r = l(r) }
     return r
+
+    // return base.differentiableReduce(input) { (input: Base.Element.Input, layer: Base.Element) -> Base.Element.Output in layer(input) }
+    // return base.differentiableReduce(input) { input, layer in layer(input) }
+/*
+    var result = input
+    for i in withoutDerivative(at: base.indices) {
+      result = base[i](result)
+    }
+    return result
+*/
   }
+
+/*
+  @usableFromInline
+  @derivative(of: callAsFunction, wrt: (self, input))
+  internal func vjpCallAsFunction(
+    _ input: Base.Element.Input
+  ) -> (
+    value: Base.Element.Output,
+    pullback: (Base.Element.Output.TangentVector) -> (TangentVector, Base.Element.Input.TangentVector)
+  ) {
+    var result = input
+    var pullbacks: [Base.Element.Backpropagator] = []
+    let count = base.count
+    pullbacks.reserveCapacity(count)
+    for i in withoutDerivative(at: base.indices) {
+      let (newResult, pb) = valueWithPullback(at: base[i], result) { $0($1) }
+      result = newResult
+      pullbacks.append(pb)
+    }
+    func pullback(_ tangent: (Base.Element.Output.TangentVector))
+      -> (TangentVector, Base.Element.Input.TangentVector) {
+      guard let count > 0 else {
+        return (self.zeroTangentVector, input.zeroTangentVector)
+      }
+      var resultTangent = tangent
+      var elementTangents = TangentVector(base: .init())
+      elementTangents.base.reserveCapacity(count)
+      for pullback in pullbacks.reversed() {
+        let (newResultTangent, elementTangent) = pullback(resultTangent)
+        resultTangent = newResultTangent
+        elementTangents.base.insert(elementTangent, at: 0)
+      }
+      return (elementTangents, resultTangent)
+    }
+    return (result, pullback)
+  }
+*/
 
   // TODO: How is this documented?
   public init(@LayerBuilder layers: () -> Self) {
@@ -46,7 +146,7 @@ public struct SequentialComposition<Base: Collection>
   }
 }
 
-extension Collection where Element : Layer, Element.Input == Element.Output
+extension Collection where Self: Differentiable, Element: Layer, Element.Input == Element.Output
 {
   /// A sequential composition of the elements of `self`.
   ///
